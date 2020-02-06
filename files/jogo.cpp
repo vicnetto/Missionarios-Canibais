5f43a646032da43e23d646eb13bd73b71f0c59e2
vicnetto <victorhmnetto@gmail.com>
2020-02-06 18:21:48 -0300

    Terminando de ajeitar todas as letras, todas estão corretamente posicionadas porém ainda não resposdem. Basicamente, a estrutura do menu está quase pronta.

--- files/jogo.cpp
+++ files/jogo.cpp
@@ -6,10 +6,13 @@
 
 int Jogo::mainMenu () {
 
-    Phrase missecan ("Missionarios e Canibais", 60, sf::Color::Green, sf::Vector2f(WIDTH * 4 / 13, HEIGHT / 6)); //Colocando a frase título;
-    Phrase jogar ("Jogar", 30, sf::Color::Red, sf::Vector2f (WIDTH * 2 / 3, HEIGHT / 2));
+    Phrase missecan ("Missionarios e Canibais", 60, sf::Color::Blue, sf::Vector2f(WIDTH * 4 / 13, HEIGHT / 6)); //Colocando a frase título;
+    Phrase jogar ("Jogar", 40, sf::Color::Cyan, sf::Vector2f (WIDTH * 4 / 9, HEIGHT / 2)); //Colocando as três opções abaixo, que podem ser escolhidasd no menu;
+    Phrase sair ("Sair", 40, sf::Color::Cyan, sf::Vector2f (WIDTH * 4 / 8.7, HEIGHT * 2 / 2.5));
+    Phrase comojogar ("Como jogar", 40, sf::Color::Cyan, sf::Vector2f (WIDTH * 4 / 9.9, HEIGHT * 2 / 3.1));
 
-    if (!missecan.setFont("archives/FFF_Tusj.ttf")) //Verificando se ela consegue colocar a fonte;
+    //Aqui é a verificação se existem os arquivos das fontes;
+    if (!missecan.setFont("archives/FFF_Tusj.ttf") || !jogar.setFont("archives/good times rg.ttf") || !sair.setFont("archives/good times rg.ttf") || !comojogar.setFont("archives/good times rg.ttf"))
     {
         std::cout << "\n\n @@@@@@ Error trying to access the file." << std::endl;
 
@@ -35,6 +38,9 @@ int Jogo::mainMenu () {
         window.clear(sf::Color::Black); //Limpa a tela e coloca uma cor inicial;
 
         window.draw(missecan.text); //Escreve a palavra depois de ter limpado;
+        window.draw(jogar.text);
+        window.draw(sair.text);
+        window.draw(comojogar.text);
 
         window.display(); //Mostra novamente o que está sendo printado na tela;
     }
